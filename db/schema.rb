7dcf4b57ccf78ee91dd2412f53f67e98c0732131
Tulio Coelho <tulioopc@gmail.com>
2016-04-21 19:02:15 -0300

    Revert "Modeling gamification"

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20160421145347) do
 
   create_table "gamifications", force: :cascade do |t|
