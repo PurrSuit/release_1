5050e7e43fd241f035308a68ad547aebcbb422fb
Tulio Coelho <tulioopc@gmail.com>
2016-04-21 09:35:10 -0300

    Doing User controller with methods and routes

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -5,43 +5,20 @@ class UserController < ApplicationController
   end
 
   def show
-    users = User.where(params[:id])
-      user = nil
-        if (users.length > 0)
-            user = users.first
-        else
-            raise "error"
-        end
-        render json: user
   end
 
   def new
-    user = User.new
   end
 
   def create
-    user = User.new(get_params)
-    if @user.save
-        render json: user
-      else
-        render action: :new
-end
   end
 
   def edit
-    
   end
 
   def update
-
   end
 
   def delete
-
   end
-
-  private
-  def get_params
-        params.require(:user).permit(:nome,:email,:password)
-    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,8 @@
 Rails.application.routes.draw do
   get 'users/all' => 'user#all', format: :json
   get 'users/new' => 'user#new'
-  post 'users/create' => 'user#create', format: :json
-  get 'users/:id' => 'user#show', format: :json
+  get 'users/create' => 'user#create', format: :json
+  get 'users/:id' => 'user#show', format :json
   get 'users/:id/update' => 'user#update', format: :json, as: :users_update
   delete 'users/:id' => 'user#delete', as: :users_delete
-
-	get 'gamifications/all' => 'gamification#all', format: :json
-
-	get 'achievements/all' => 'achievement#all', format: :json
-	get 'achievements/new' => 'achievement#new', format: :json, as: :achievements_new
-	post 'achievements/create' => 'achievement#create', as: :achievements
-	get 'achievements/:id' => 'achievement#show', format: :json
-		
-	get 'quests/all' => 'quest#all', format: :json
-	get 'quests/new' => 'quest#new', format: :json
-	post 'quests/create' => 'quest#create', as: :quests
-	get 'quests/:id' => 'quest#show', format: :json
-	get 'quests/:id/update' => 'quest#update', format: :json, as: :quests_update
-	delete 'quests/:id' => 'quest#delete', as: :quests_delete
 end
