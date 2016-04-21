8fa2586a523d7a3edf08ca28ec02333ed7dc8297
Thiago Felix <thiago.19.rafael@gmail.com>
2016-04-21 20:13:24 -0300

    Creating Gamification model

--- db/migrate/20160421230411_create_gamifications.rb
+++ db/migrate/20160421230411_create_gamifications.rb
@@ -1,13 +1,11 @@
 class CreateGamifications < ActiveRecord::Migration
   def change
     create_table :gamifications do |t|
-			t.integer :actable_id
-			t.string :actable_type
 
 			t.string :name
 			t.integer :experience_points
 			t.string :description
-			
+
       t.timestamps null: false
     end
   end
