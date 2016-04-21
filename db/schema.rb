e593a48f308fcf1fea64dd9f73933a97d9c39d99
Tulio Coelho <tulioopc@gmail.com>
2016-04-21 09:26:47 -0300

    Doing migration for both models

--- db/schema.rb
+++ db/schema.rb
@@ -11,18 +11,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160427020103) do
 
-  create_table "achievements", force: :cascade do |t|
-  end
-
-  create_table "gamifications", force: :cascade do |t|
-    t.string   "name"
-    t.integer  "experience_points"
-    t.string   "description"
-    t.datetime "created_at",        null: false
-    t.datetime "updated_at",        null: false
-  end
+ActiveRecord::Schema.define(version: 20160421122248) do
 
   create_table "people", force: :cascade do |t|
     t.integer  "actable_id"
@@ -34,9 +24,6 @@ ActiveRecord::Schema.define(version: 20160427020103) do
     t.string   "age"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
-  end
-
-  create_table "quests", force: :cascade do |t|
   end
 
   create_table "users", force: :cascade do |t|
