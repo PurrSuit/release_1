dca35fe7ea9b592085cdbf2221648084cb1cf218
Stefania <stefaniabezerra95@gmail.com>
2016-04-27 15:26:15 -0300

    delete_user

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,6 @@ class User < ActiveRecord::Base
   before_save { self.email = email.downcase }
   validates :email, presence: true
 
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 8 }
 
 end
