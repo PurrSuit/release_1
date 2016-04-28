2ac2c3a58cb32cb5e0321bdb27988599cd29e474
Tulio Coelho <tulioopc@gmail.com>
2016-04-28 15:15:18 -0300

    Quest view to json

--- app/controllers/quest_controller.rb
+++ app/controllers/quest_controller.rb
@@ -1,12 +1,11 @@
 class QuestController < ApplicationController
 
 	def all
-		#render json: Gamification.all
-		@quests = Quest.all
+		render json: Quest.all
 	end
 
 	def show
-		@quest = Quest.find(params[:id])
+		render json: Quest.find(params[:id])
 	end
 
 	def new
@@ -23,11 +22,15 @@ class QuestController < ApplicationController
 	end
 
 	def edit
+
 		@quest = Quest.find(params[:id])
+		render 'edit'
+
 	end
 
 	def update
 		@quest = Quest.find(params[:id])
+
 		if @quest.update(quest_params)
 			redirect_to @quest
 		else
@@ -39,7 +42,10 @@ class QuestController < ApplicationController
 		id_quest = params[:id]
 		quests = Quest.where(id: id_quest)
 		quest = quests.first
-		redirect_to :quests_all
+			redirect_to :quests_all
+		else
+			render 'edit'
+		end
 	end
 
 	private
@@ -47,5 +53,3 @@ class QuestController < ApplicationController
 	def quest_params
 		params.require(:quest).permit(:name, :experience_points, :description)
 	end
-
-end
