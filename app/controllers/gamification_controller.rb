2d4df4dc887c8d9ee0dac4ef60699fcea2acbdfe
Thiagotrfm <thiago.19.rafael@gmail.com>
2016-05-02 14:08:30 -0300

    Conflicts conrrections

--- app/controllers/gamification_controller.rb
+++ app/controllers/gamification_controller.rb
@@ -3,43 +3,4 @@ class GamificationController < ApplicationController
 		@gamifications = Gamification.all
 	end
 
-	def show
-		@gamification = Gamification.find(params[:id])
-		id_gamification = params[:id]
-		gamifications = Gamification.where(id: id_gamification)
-		gamification = nil
-	end
-
-	def new
-		@gamification = Gamification.new
-	end
-
-	def create
-		@gamification = Gamification.new(gamification_params)
-		if @gamification.save
-			redirect_to :gamifications_all
-		end
-	end
-
-	def edit
-
-	end
-
-	def update
-
-	end
-
-	def delete
-		id_gamification = params[:id]
-		gamifications = Gamification.where(id: id_gamification)
-		gamification = gamifications.first
-		redirect_to :gamifications_all
-	end
-
-	private
-
-	def gamification_params
-		params.require(:gamification).permit(:name, :experience_points, :description)
-	end
-
 end
