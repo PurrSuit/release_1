350139736cb4ad3f6710af6980b006b031720714
Tulio Coelho <tulioopc@gmail.com>
2016-05-06 14:45:07 -0300

    Adding first test and rolling back migrations

--- db/schema.rb
+++ db/schema.rb
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 20160506150419) do
     t.string   "email"
     t.string   "cpf"
     t.string   "age"
+    t.integer  "uf_id"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
   end
@@ -63,8 +64,8 @@ ActiveRecord::Schema.define(version: 20160506150419) do
 
   create_table "users", force: :cascade do |t|
     t.string  "password_digest"
-    t.integer "experience_points"
-    t.boolean "role_admin"
+    t.integer "experience_points", default: 0,     null: false
+    t.boolean "role_admin",        default: false, null: false
   end
 
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @user = User.new(name: "Usuario Teste", email: "a@b.com", password: "testeteste",
+    password_digest: "testeteste", gender:"Masculino", age: 21, role_admin: false, experience_points: 0)
+  end
+
+  test "setup should be valid" do
+    assert @user.valid?
+  end
+
+  
+
+
 end
