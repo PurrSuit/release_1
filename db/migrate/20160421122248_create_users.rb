55cfffaf4881b5f5c3ad0b89cd9dffadc7dfe190
Tulio Coelho <tulioopc@gmail.com>
2016-05-06 14:44:25 -0300

    Adding default values for role_admin and experience_points

--- db/migrate/20160421122248_create_users.rb
+++ db/migrate/20160421122248_create_users.rb
@@ -2,8 +2,8 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string :password_digest
-      t.integer :experience_points
-      t.boolean :role_admin
+      t.integer :experience_points, :null => false, :default => 0
+      t.boolean :role_admin, :null => false, :default => false
 
     end
   end
