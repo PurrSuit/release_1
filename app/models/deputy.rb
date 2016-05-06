e1fac0117fb2f372c9097c7070f3277b81f6884b
Tulio Coelho <tulioopc@gmail.com>
2016-05-06 16:05:57 -0300

    basic validations for deputy model

--- app/models/deputy.rb
+++ app/models/deputy.rb
@@ -1,3 +1,13 @@
 class Deputy < ActiveRecord::Base
   acts_as :person
+
+  validates_numericality_of :age
+  validates_numericality_of :registration
+
+  validates :name, presence: true, length: {maximum: 50}
+  validates :age, presence: true
+  validates :gender, presence: true
+  validates :registration, presence: true, uniqueness: true
+  validates :legislation_situation, presence: true, length: {maximum: 100}
+
 end
