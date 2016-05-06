cf3a7ed2458a2149c2e73f0b00725495a23128c6
Tulio Coelho <tulioopc@gmail.com>
2016-05-06 16:05:57 -0300

    basic validations for deputy model

--- app/models/party.rb
+++ app/models/party.rb
@@ -1,9 +1,9 @@
 class Party < ActiveRecord::Base
 	belongs_to :uf
 	#has_many :deputies
-	
+
 	validates :name, presence: true, length: { minimum: 10 }, uniqueness: true
 	validates :initials, presence: true, length: { minimum: 2 }, uniqueness: true
 	validates :uf_id , presence: true
-		
+
 end
