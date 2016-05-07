fd68e9c8b350298e85230859b21e46c817256f01
Tulio Coelho <tulioopc@gmail.com>
2016-05-07 18:35:57 -0300

    Active Record Associations with UF and Party

--- app/models/uf.rb
+++ app/models/uf.rb
@@ -1,10 +1,11 @@
 class Uf < ActiveRecord::Base
-	# has_many :people, :parties
+	has_many :deputies
+	has_many :users
 
 	validates :name, presence: true, length: { minimum: 4, maximum: 20 }, uniqueness: true
 	validates :initials, presence: true, length: { minimum: 2, maximum: 2 }, uniqueness: true
 	before_save :uppercase_initials
-	
+
 	def uppercase_initials
 		self.initials.upcase!
 	end
