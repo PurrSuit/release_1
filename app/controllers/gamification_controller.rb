2e36cd070b823cab69ba5d9e05f4942dd2d1333c
Indiara <indiara96d@gmail.com>
2016-05-07 12:27:27 -0300

    hehe

--- app/controllers/gamification_controller.rb
+++ app/controllers/gamification_controller.rb
@@ -2,7 +2,11 @@ class GamificationController < ApplicationController
 	def all
 		@gamifications = Gamification.all
 	end
+<<<<<<< HEAD
 	def show
 		@gamification = Gamification.find(params[:id])
 	end
+=======
+
+>>>>>>> f1024f8... hehe
 end
