3f14c5b2df445cc69d5a18ae54ccc6ec0a34fd76
Matheus Oliveira <matheus_oliveira30@hotmail.com>
2016-05-10 17:38:52 -0300

    Added Deputy Ionic view

--- purrsuit_view/www/js/app.js
+++ purrsuit_view/www/js/app.js
@@ -1,15 +1,7 @@
-// Ionic Starter App
-
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-// 'starter.controllers' is found in controllers.js
 angular.module('starter', ['ionic', 'starter.controllers', 'ngResource'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
     if (window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
       cordova.plugins.Keyboard.disableScroll(true);
@@ -76,7 +68,6 @@ angular.module('starter', ['ionic', 'starter.controllers', 'ngResource'])
     }
   })
 
-
   // Listing all users
   .state('app.users', {
     url: "/users",
@@ -98,16 +89,28 @@ angular.module('starter', ['ionic', 'starter.controllers', 'ngResource'])
     }
   })
 
-  .state('app.browse', {
-      url: '/browse',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/browse.html'
-        }
+  // Listing all deputies
+  .state('app.browseDeputies', {
+    url: '/browseDeputies',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/deputies/browseDeputies.html',
+        controller: 'DeputiesCtrl'
       }
-    })
+    }
+  })
 
-    // if none of the above states are matched, use this as the fallback
-    // Deputys view is 'browse'
+  // Listing deputy's details
+  .state('app.deputy', {
+    url: '/deputy/:deputyId',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/deputies/deputy.html',
+        controller: 'DeputyCtrl'
+      }
+    }
+  })
+
+  // Deputys view is 'browseDeputies'
   $urlRouterProvider.otherwise('/app/login');
 });
---
+++ purrsuit_view/www/js/deputiesController.js
@@ -0,0 +1,28 @@
+angular.module('starter')
+
+.controller('DeputiesCtrl', function($scope, ServerDeputies) {
+  ServerDeputies.get(function(data) {
+      console.log("SERVICES: Getting Deputies data from server...")
+      $scope.deputies = data.deputy;
+    },
+    function(error) {
+      alert("Não foi possível estabelecer conexão com o servidor...");
+      console.log("SERVICES: ERROR in Deputies data from server...");
+    })
+})
+
+.controller('DeputyCtrl', function($scope, $stateParams, ServerFindDeputy) {
+  var findId = $stateParams.deputyId;
+  ServerFindDeputy.get({
+      id: findId
+    },
+    function(data) {
+      console.log("SERVICES: Getting Deputy (Id: " + findId + ") data from server...");
+      $scope.deputy = data.deputy;
+    },
+    function(error) {
+      alert("Não foi possível estabelecer conexão com o servidor...");
+      console.log("SERVICES: ERROR in getting Deputy (Id: " + findId + ") data from server...");
+    }
+  )
+})
