15f610bc0c3210e9de6af6b8eb1a9a430e173e8c
Matheus Oliveira <matheus_oliveira30@hotmail.com>
2016-05-10 17:38:52 -0300

    Added Deputy Ionic view

--- purrsuit_view/www/js/services.js
+++ purrsuit_view/www/js/services.js
@@ -5,6 +5,16 @@ angular.module('starter.services', ['ngResource'])
   return $resource('http://localhost:3000/login/signin')
 })
 
+.factory('ServerDeputies', function($resource){
+  return $resource('http://localhost:3000/deputies/all')
+})
+
+.factory('ServerFindDeputy', function($resource){
+  return $resource('http://localhost:3000/deputies/:id',{
+    id: "@id"
+  })
+})
+
 .factory('ServerQuests', function($resource) {
   return $resource('http://localhost:3000/quests/all.json')
 })
