f244bf9e5fac96d4e9064e035406465d6518c392
Ramon Sales <ramon.silvasales@gmail.com>
2016-05-11 00:46:20 -0300

    fixing login controller

--- purrsuit_view/www/js/loginController.js
+++ purrsuit_view/www/js/loginController.js
@@ -6,5 +6,19 @@ angular.module('starter')
     console.log(user)
     LogInFactory.get(user);
   }
+  $scope.signIn = function(user){
+    LogInFactory.get(user, function(user) {
+      $ionicPopup.alert({
+        title: 'Sucesso :D',
+        template: 'Login efetuado'
+      });
+      console.log(user);
+    }, function(error) {
+      $ionicPopup.alert({
+        title: 'Erro :(',
+        template: 'Falha no login'
+      });
+    });
+  }
 }
 )
