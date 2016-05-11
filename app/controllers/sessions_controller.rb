41884e49d2c66266f13fbb10cb57816056733973
Ramon Sales <ramon.silvasales@gmail.com>
2016-05-11 17:46:42 -0300

    login and signup repaired

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,12 +5,17 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:email])
-    if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
-      render json: user
-      raise "login successfull!"
-    else
-      raise "login failed!"
+    begin
+      if user && user.authenticate(params[:password])
+        session[:user_id] = user.id
+        puts "login successfull!"
+        puts user
+        render json: user
+      else
+        raise "login failed!"
+      end
+    rescue RuntimeError => error_login
+      puts "#{error_login}"
       redirect_to 'login'
     end
   end
