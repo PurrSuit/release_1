f777e9064c431c82b384e1e27e74e28d74d772b2
Tulio Coelho <tulioopc@gmail.com>
2016-05-11 14:04:46 -0300

    Adding some gamification validations

--- app/models/gamification.rb
+++ app/models/gamification.rb
@@ -1,4 +1,8 @@
 class Gamification < ActiveRecord::Base
 	actable
-	validates_uniqueness_of :name
+	validates_numericality_of :experience_points
+
+	validates :name, presence: true, length: {maximum: 50}, uniqueness: true
+	validates :description, presence: true, length: {maximum: 140}, uniqueness: true
+	validates :experience_points, presence: true
 end
