917fdd780545a01415cbac157e8547ef0b653690
brunobrg <bbraganca1991@gmail.com>
2016-05-12 00:18:20 -0300

    Created way of populating the database if the webservice isnt available

--- app/models/party.rb
+++ app/models/party.rb
@@ -16,7 +16,15 @@ class Party < ActiveRecord::Base
     response = Net::HTTP.start(url.host, url.port) {|http|
       http.request(request)
     }
-    xml_doc = Nokogiri::XML(response.body)
+		if response.kind_of? Net::HTTPSuccess
+      xml_doc = Nokogiri::XML(response.body)
+      puts "Alimentando base de dados dos PARTIDOS a partir do webservice..."
+    else
+      xml_doc = Nokogiri::XML(File.open("xml/parties.xml"))
+      puts "WebService dos PARTIDOS inacessível."
+      puts "Alimentado base de dados dos PARTIDOS a partir de backup..."
+    end
+
     xml_doc.xpath("//partido").each do |party|
       initials = party.elements[1].text.to_s
       name = party.elements[2].text.to_s
@@ -31,5 +39,15 @@ class Party < ActiveRecord::Base
 			end
     end
   end
+
+	def self.save_xml
+		url = URI.parse("http://www.camara.gov.br/SitCamaraWS/Deputados.asmx/ObterPartidosCD")
+    request = Net::HTTP::Get.new(url.to_s)
+    response = Net::HTTP.start(url.host, url.port) {|http|
+      http.request(request)
+    }
+    xml_doc = Nokogiri::XML(response.body)
+		  File.open('xml/parties.xml', 'w') {|f| f.write xml_doc}
+	end
 
 end
