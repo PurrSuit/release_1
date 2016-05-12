bc753318c445b4939a4b9290a0b5a098afef6bdd
Tulio Coelho <tulioopc@gmail.com>
2016-05-12 00:38:52 -0300

    fixing sintaxe errors

--- app/serializers/deputy_serializer.rb
+++ app/serializers/deputy_serializer.rb
@@ -1,3 +1,3 @@
 class DeputySerializer < ActiveModel::Serializer
-  attributes :id, :name, :deputy_name, :email, :gender :age, :registration, :legislation_situation, :image_path
+  attributes :id, :name, :deputy_name, :email, :gender, :age, :registration, :legislation_situation, :image_path
 end
